d949f3cc8e03883ffd53a946d55d1d24cd6cdcec
Kent Ross <k@mad.cash>
2023-01-06 17:46:58 -0800

    always find token in the same folder as the script

--- uhoh.sh
+++ uhoh.sh
@@ -7,8 +7,10 @@ json_escape () {
 
 json_message=$(json_escape "$*")
 json_push='{"type":"note","title":"uh oh","body":'"${json_message}"'}'
+token_location="$(dirname $0)/.pushbullet-token"
+token=$(cat $token_location)
 
 curl https://api.pushbullet.com/v2/pushes -X POST \
   -H "Content-Type: application/json" \
-  -H "Access-Token: $(cat ./.pushbullet-token)" \
+  -H "Access-Token: ${token}" \
   --data-raw "$json_push"
