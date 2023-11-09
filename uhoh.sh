f40d988c80e0312698f4be67efaf9005a66eee00
Kent Ross <k@mad.cash>
2023-11-08 23:59:41 -0800

    don't show curl progress bar

--- uhoh.sh
+++ uhoh.sh
@@ -10,7 +10,9 @@ json_push='{"type":"note","title":"uh oh","body":'"${json_message}"'}'
 token_location="$(dirname $(readlink -f $0))/.pushbullet-token"
 token=$(cat $token_location)
 
-curl https://api.pushbullet.com/v2/pushes -X POST \
+curl https://api.pushbullet.com/v2/pushes \
+  --silent \
+  -X POST \
   -H "Content-Type: application/json" \
   -H "Access-Token: ${token}" \
   --data-raw "$json_push"
